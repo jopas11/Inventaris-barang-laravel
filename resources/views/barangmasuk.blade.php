0ab5b519e3785ef01c3bebf055855e815f7815c7
Haikal1305 <haikaljimmi15@gmail.com>
2026-07-25 15:40:53 +0700

    CFDA

--- resources/views/barangmasuk.blade.php
+++ resources/views/barangmasuk.blade.php
@@ -15,7 +15,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                <span class="text-lg font-semibold text-gray-700">Daftar Barang Masuk</span>
+                <span class="text-lg font-semibold text-gray-700">Daftar Barang Masukk</span>
             </div>
             <table id="tabelBarangMasuk" class="display w-full text-sm text-gray-700">
                 <thead class="bg-gray-100">
